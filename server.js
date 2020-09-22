899588f6a098ed799e7bbfc7839a7e383a9a8d2f
musabbihanoor <musabbihanoor33@gmail.com>
2020-09-22 14:31:00 -0700

    secondcommit

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
+const connectDB = require("./config/db");
 const PORT = process.env.PORT || 5000;
 
+connectDB();
 app.get("/", (req, res) => res.send("API Running"));
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
