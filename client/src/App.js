de4c21c05ab27f4e9d1d57fda88ddb2ab27fa0c4
musabbihanoor <musabbihanoor33@gmail.com>
2020-10-11 09:17:06 -0700

    commit

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,8 @@ import Alert from "./component/layout/Alert";
 import Dashboard from "./component/dashboard/Dashboard";
 import CreateProfile from "./component/profile-form/CreateProfile";
 import EditProfile from "./component/profile-form/EditProfile";
+import AddExperience from "./component/profile-form/AddExperience";
+import AddEducation from "./component/profile-form/AddEducation";
 import PrivateRoute from "./component/routing/PrivateRoute";
 // Redux
 import { Provider } from "react-redux";
@@ -47,6 +49,16 @@ const App = () => {
                 exact
                 path='/edit-profile'
                 component={EditProfile}
+              />
+              <PrivateRoute
+                exact
+                path='/add-experience'
+                component={AddExperience}
+              />
+              <PrivateRoute
+                exact
+                path='/add-education'
+                component={AddEducation}
               />
             </Switch>
           </section>
