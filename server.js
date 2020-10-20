77d73e571d82fb70ab9224f2f3b0790e96ba6c34
musabbihanoor <musabbihanoor33@gmail.com>
2020-10-20 04:10:03 -0700

    commit

--- server.js
+++ server.js
@@ -1,18 +1,29 @@
 const express = require("express");
 const app = express();
 const connectDB = require("./config/db");
-const PORT = process.env.PORT || 5000;
+const path = require('path');
 
 //connect DB
 connectDB();
 
 app.use(express.json({ extended: false }));
-app.get("/", (req, res) => res.send("API Running"));
 
 //define routes
 app.use("/api/users", require("./routes/api/users"));
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/post", require("./routes/api/post"));
+
+// Serve static assets in production
+if(process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+}
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
